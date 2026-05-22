77c1291cb942b79c4075707130a2b923d799e959
Hitozz <118142018+hitozz0@users.noreply.github.com>
2026-05-22 16:04:32 +0800

    create feature subscription

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\CustomersController;
+use App\Http\Controllers\Api\SubsController;
 
 Route::apiResource("services", ServiceController::class);
 Route::patch("services/{service}/activate", [ServiceController::class,"activate",]);
@@ -12,5 +13,9 @@ Route::patch("services/{service}/deactivate", [ServiceController::class,"deactiv
 Route::apiResource("customers", CustomersController::class);
 Route::patch("customers/{id}/activate", [CustomersController::class,"activate",]);
 Route::patch("customers/{id}/deactivate", [CustomersController::class,"deactivate",]);
+
+Route::get("subscription", [SubsController::class,"index",]);
+Route::post("subscription", [SubsController::class,"store",]);
+Route::patch("subscription/changeStatus/{id}/{status}", [SubsController::class,"changeStatus",]);
 
 
